048f8c1b08e8b147a073006579f4430604e2c8c9
LohithaNarayani <63665560+LohithaNarayani@users.noreply.github.com>
2021-03-22 11:48:03 +0530

    Update HomePageTest.java
    22.03.2021

--- src/test/java/test/HomePageTest.java
+++ src/test/java/test/HomePageTest.java
@@ -5,18 +5,29 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import pages.HomePage;
+import utilities.ExcelUtils;
 
 public class HomePageTest {
 
 	HomePage homePage;
+	ExcelUtils eu;
 
 	@BeforeSuite
 	public void beforeSuite() {
+		System.out.println("In testLaunchHomePage");
 		homePage = new HomePage();
+		eu = new ExcelUtils();
 	}
 
 	@Test
 	public void testLaunchHomePage() {
+		
+		try {
+			eu.getExcelData("PolicyNumber",3);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 
 		homePage.launchHomePage();
 	}
